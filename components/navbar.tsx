e5aaecb125470e89acb4a0caa9a66b19983354d3
saadmanfarhad <saadmanheavy@gmail.com>
2021-03-05 19:19:58 +0600

    Added logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,13 +13,13 @@ export const Navbar = () => {
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
               <img
-                className="block lg:hidden h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                className="block lg:hidden h-12 w-16"
+                src="/logo-mobile.png"
                 alt="Workflow"
               />
               <img
-                className="hidden lg:block h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                className="hidden lg:block h-14 w-32"
+                src="/logo.png"
                 alt="Workflow"
               />
             </div>
