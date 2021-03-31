37ba55bdcd34026d12caa8fa4328ab2940dac2f1
saadmanfarhad <saadmanheavy@gmail.com>
2021-03-31 15:14:46 +0600

    Implemented data fetching using swr

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,43 @@
+import React, { useState } from "react";
 import Head from "next/head";
 import { Layout } from "../components/layout.tsx";
 import { SearchBar } from "../components/searchbar.tsx";
 import { CardSkeleton } from "../components/skeleton.tsx";
 import { Card } from "../components/card.tsx";
-import useFetchData from "../hooks/useFetchData";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const Home = (props) => {
-  const [state, setUrl] = useFetchData("", { hits: [] });
-  console.log(state.data);
+  const [url, setUrl] = useState("");
+  const { data, error } = useSWR(url, fetcher);
 
   const search = (query) => {
     if (query.length) {
       const url = `https://api.themoviedb.org/3/search/movie?api_key=4f17f3213e737992b22b4f7ebc04fc85&language=en-US&query=${query}&page=1&include_adult=false`;
       setUrl(url);
+    }
+  };
+
+  const process = () => {
+    if (error) {
+      <h1>Error!</h1>
+    }
+
+    if (!data) {
+      return (
+      url.length &&  <div className="space-y-6">
+          <CardSkeleton />
+          <CardSkeleton />
+          <CardSkeleton />
+        </div>
+      );
+    }
+
+    if (data) {
+      console.log("data", data);
+      const items = data.results.filter((movies) => movies.overview.length);
+      return items.length && items.map((info, idx) => <Card data={info} />);
     }
   };
 
@@ -28,17 +53,7 @@ const Home = (props) => {
             <SearchBar onChange={search} />
           </div>
           <div className="mt-2 w-full">
-            {state.isLoading ? (
-              <div className="space-y-6">
-                <CardSkeleton />
-                <CardSkeleton />
-                <CardSkeleton />
-              </div>
-            ) : (
-              state.data.results?.length &&
-              state.data.results.map((res, idx) => <Card data={res} />)
-            )}
-            <CardSkeleton />
+            {process()}
           </div>
         </div>
       </Layout>
