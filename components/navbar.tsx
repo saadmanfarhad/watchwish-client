c98ed33225659c905efbf731670780e8fbf169b6
saadmanfarhad <saadmanheavy@gmail.com>
2021-04-23 15:44:15 +0600

    Overhauling navbar done

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,83 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/client";
 import { useTheme } from "next-themes";
 
 export const Navbar = () => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  const [session, loading] = useSession();
+
+  console.log(session);
+
   useEffect(() => setMounted(true), []);
 
   return (
-    <nav className="bg-white dark:bg-gray-900 h-16">
+    <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/*<!-- Mobile menu button-->*/}
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+              onClick={() => setShowMobileMenu(!showMobileMenu)}
+            >
+              <span className="sr-only">Open main menu</span>
+              {/*<!--
+            Icon when menu is closed.
+
+            Heroicon name: outline/menu
+
+            Menu open: "hidden", Menu closed: "block"
+          -->*/}
+              {showMobileMenu ? (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+
+              {/*<!--
+            Icon when menu is open.
+
+            Heroicon name: outline/x
+
+            Menu open: "block", Menu closed: "hidden"
+          -->*/}
+            </button>
+          </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
               <Link href={`/`}>
@@ -22,18 +89,49 @@ export const Navbar = () => {
               </Link>
               <Link href={`/`}>
                 <img
-                  className="hidden cursor-pointer lg:block h-14 w-32"
+                  className="hidden cursor-pointer lg:block h-12 w-auto"
                   src="/logo.png"
                   alt="Workflow"
                 />
               </Link>
             </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4 mt-2">
+                {/*<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->*/}
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  aria-current="page"
+                >
+                  Dashboard
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Team
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Projects
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Calendar
+                </a>
+              </div>
+            </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button
-              aria-label="Toggle Dark Mode"
-              type="button"
-              className="bg-gray-200 hover:bg-gray-300 dark:bg-gray-800  dark:hover:bg-gray-700 transition-all rounded flex items-center justify-center h-7 w-7"
+              className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
             >
               {mounted && (
@@ -41,7 +139,7 @@ export const Navbar = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
-                  className="h-4 w-4 text-gray-800 dark:text-gray-200"
+                  className="h-4 w-4 text-white"
                 >
                   {theme === "dark" ? (
                     <path
@@ -55,9 +153,126 @@ export const Navbar = () => {
                 </svg>
               )}
             </button>
+
+            {/*<!-- Profile dropdown -->*/}
+            <div className="ml-3 relative">
+              <div>
+                {session && <button
+                  type="button"
+                  className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  id="user-menu-button"
+                  aria-expanded="false"
+                  aria-haspopup="true"
+                  onClick={() => setShowUserMenu(!showUserMenu)}
+                >
+                  <span className="sr-only">Open user menu</span>
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src={session.user.avatar ? session.user.avatar : "/user.png"}
+                    alt=""
+                  />
+                </button>}
+                {!session && <Link href="/login">
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    aria-current="page"
+                    >
+                    Login
+                  </a>
+                </Link>}
+              </div>
+
+              {/*<!--
+            Dropdown menu, show/hide based on menu state.
+
+            Entering: "transition ease-out duration-100"
+              From: "transform opacity-0 scale-95"
+              To: "transform opacity-100 scale-100"
+            Leaving: "transition ease-in duration-75"
+              From: "transform opacity-100 scale-100"
+              To: "transform opacity-0 scale-95"
+          -->*/}
+              {showUserMenu ? (
+                <div
+                  className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="user-menu-button"
+                  tabIndex="-1"
+                >
+                  {/*<!-- Active: "bg-gray-100", Not Active: "" -->*/}
+                  <a
+                    href="#"
+                    className="block hover:bg-gray-200 px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="user-menu-item-0"
+                  >
+                    Your Profile
+                  </a>
+                  <a
+                    href="#"
+                    className="block hover:bg-gray-200 px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="user-menu-item-1"
+                  >
+                    Settings
+                  </a>
+                  <a
+                    href="#"
+                    className="block hover:bg-gray-200 px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="user-menu-item-2"
+                    onClick={() => signOut()}
+                  >
+                    Sign out
+                  </a>
+                </div>
+              ) : undefined}
+            </div>
           </div>
         </div>
       </div>
+
+      {/*<!-- Mobile menu, show/hide based on menu state. -->*/}
+      {showMobileMenu ? (
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            {/*<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->*/}
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              aria-current="page"
+            >
+              Dashboard
+            </a>
+
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Team
+            </a>
+
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Projects
+            </a>
+
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Calendar
+            </a>
+          </div>
+        </div>
+      ) : undefined}
     </nav>
   );
 };
